9daf6e1b1498538014825b560fe71cf114eefcc2
arek <test>
2015-04-08 00:09:18 +0200

    Solution template

---
+++ GrepEngineIntegrationTests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrepEngineIntegrationTests
+{
+    public class Class1
+    {
+    }
+}
